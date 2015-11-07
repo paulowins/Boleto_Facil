6fd60b4f92521c4b103b98a4a89ccc241c00ff8a
paulowins <paulo.tec.informatica@gmail.com>
2015-11-07 10:33:43 -0200

    Primeiro Commit
    Base do projeto

---
+++ app/src/main/java/jocimar_paulo/ifsp/boletofacil/ActSplashArt.java
@@ -0,0 +1,13 @@
+package jocimar_paulo.ifsp.boletofacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActSplashArt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_splash_art);
+    }
+}

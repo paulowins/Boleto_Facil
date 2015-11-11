e09a178db3f8a65ac486377629f257f809ef615c
JocimarSVN <jocimarweb@gmail.com>
2015-11-11 13:02:06 -0200

    Primeiro Commit - Jocimar Criação da Tela GeraBoleto

---
+++ app/src/main/java/jocimar_paulo/ifsp/boletofacil/telas/GeraBoleto.java
@@ -0,0 +1,15 @@
+package jocimar_paulo.ifsp.boletofacil.telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jocimar_paulo.ifsp.boletofacil.R;
+
+public class GeraBoleto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gera_boleto);
+    }
+}

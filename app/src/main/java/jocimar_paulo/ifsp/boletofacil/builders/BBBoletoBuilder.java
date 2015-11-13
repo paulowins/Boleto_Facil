2ab7787090491f35f2ac85644c8ca3aba1ab1806
paulowins <paulo.tec.informatica@gmail.com>
2015-11-13 16:35:49 -0200

    BBBoletoBuilder finalizado.

--- app/src/main/java/jocimar_paulo/ifsp/boletofacil/builders/BBBoletoBuilder.java
+++ app/src/main/java/jocimar_paulo/ifsp/boletofacil/builders/BBBoletoBuilder.java
@@ -12,16 +12,14 @@ import jocimar_paulo.ifsp.boletofacil.dados.Conexao;
  */
 public class BBBoletoBuilder extends BoletoBuilder {
 
-
-
     @Override
     public void buildSacado() {
-        boleto.Sacado = Conexao.nomeSacado + " - " +Conexao.cpfSacado;
+        boleto.Sacado = Conexao.nomeSacado + " - " + Conexao.cpfSacado;
     }
 
     @Override
     public void buildCedente() {
-        switch (Conexao.opcaoMenu){
+        switch (Conexao.opcaoMenu) {
             case 1:
             case 4:
                 boleto.Cedente = "Prefeitura Municipal - Meio Ambiente";
@@ -41,21 +39,21 @@ public class BBBoletoBuilder extends BoletoBuilder {
     @Override
     public void buildValor() {
         //Constroi o valor com base na opção escolhida
-        switch (Conexao.opcaoMenu){
+        switch (Conexao.opcaoMenu) {
             case 1:
-                boleto.Valor = 33.90f;
+                boleto.Valor = 33.95f;
                 break;
             case 2:
-                boleto.Valor = 540;
+                boleto.Valor = 540.55f;
                 break;
             case 3:
-                boleto.Valor = 90;
+                boleto.Valor = 90.01f;
                 break;
             case 4:
-                boleto.Valor = 230.60f;
+                boleto.Valor = 230.69f;
                 break;
             case 5:
-                boleto.Valor = 200f;
+                boleto.Valor = 200.01f;
                 break;
             case 6:
                 boleto.Valor = 300.99f;
@@ -73,23 +71,78 @@ public class BBBoletoBuilder extends BoletoBuilder {
     @Override
     public void buildNossoNumero() {
         Random geraNumero = new Random();
-        int numero = (geraNumero.nextInt(8)+1)*10000;
+        int numero = (geraNumero.nextInt(8) + 1) * 10000;
         boleto.nossoNumero = numero;
     }
 
     @Override
     public void buildCodBarras() {
         StringBuilder sb = new StringBuilder();
-        sb.append("001"); //Identificação do banco
-        sb.append("9"); //Codigo da moeda (9 = Real)
-        sb.append(boleto.nossoNumero); //5 digitos livres
-        sb.append("0"); //digito verificador
-        sb.append("");
+        sb.append("001."); //Identificação do banco
+        sb.append("9."); //Codigo da moeda (9 = Real)
+        sb.append(boleto.nossoNumero + "."); //5 digitos livres
+
+        int aux = boleto.nossoNumero / 10001;
+        sb.append(aux + "."); //digito verificador
+
+        Random random = new Random();
+        for (int i = 0; i < 11; i++) {
+            sb.append(random.nextInt(9)); //gera 11 digitos lives
+        }
+
+        sb.append(".");
+        for (int i = 0; i < 11; i++) {
+            sb.append(random.nextInt(9)); //gera 11 digitos lives
+        }
+        sb.append(".");
+
+        //Calcula o número de dias decorridos desde a data-base — 07/10/1997 (Febraban)
+        Calendar dataBase = Calendar.getInstance();
+        dataBase.set(1997, 10, 7); // 07/10/1997
+        sb.append(diffInDays(boleto.Vencimento.getTime(), dataBase.getTime()));  //Adiona o numero de referencia ao vencimento
+        sb.append(".");
+
+        //coloca o valor e comprementa com zeros na frente
+        String valor = String.valueOf(boleto.Valor);
+        while (valor.length() <= 10) {
+            valor = "0" + valor;
+        }
+        sb.append(valor); //10 digito  para valor
+
         boleto.CodBarras = sb.toString();
     }
 
     @Override
     public void buildObsercacao() {
+        boleto.Observacao =  "" +
+                "O boleto deve ser impresso e pago nas agencias do Banco do Brasil." +
+                " Não Aceitar após o vencimento." +
+                " Levar Comprovante no dia da solicitação.";
+    }
 
+    /**
+     * Retorna o numero de dias entre duas datas
+     *
+     * @param d1 dataMaior
+     * @param d2 DataMenor
+     * @return int numero de dias
+     */
+    private int diffInDays(Date d1, Date d2) {
+        int MILLIS_IN_DAY = 86400000;
+
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(d1);
+        c1.set(Calendar.MILLISECOND, 0);
+        c1.set(Calendar.SECOND, 0);
+        c1.set(Calendar.MINUTE, 0);
+        c1.set(Calendar.HOUR_OF_DAY, 0);
+
+        Calendar c2 = Calendar.getInstance();
+        c2.setTime(d2);
+        c2.set(Calendar.MILLISECOND, 0);
+        c2.set(Calendar.SECOND, 0);
+        c2.set(Calendar.MINUTE, 0);
+        c2.set(Calendar.HOUR_OF_DAY, 0);
+        return (int) ((c1.getTimeInMillis() - c2.getTimeInMillis()) / MILLIS_IN_DAY);
     }
 }

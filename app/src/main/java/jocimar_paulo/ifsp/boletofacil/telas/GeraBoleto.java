89e551f452bdf8276337b112e7d2ae16ec85620d
JocimarSVN <jocimarweb@gmail.com>
2015-11-14 13:21:55 -0200

    Realizado:  > a criação do design do boleto;  > a setagem dos dados para os campos do boleto;  > a filtragem e troca da logo do banco de acordo com o cod do banco setado no código de barras;

--- app/src/main/java/jocimar_paulo/ifsp/boletofacil/telas/GeraBoleto.java
+++ app/src/main/java/jocimar_paulo/ifsp/boletofacil/telas/GeraBoleto.java
@@ -2,25 +2,77 @@ package jocimar_paulo.ifsp.boletofacil.telas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import jocimar_paulo.ifsp.boletofacil.R;
-import jocimar_paulo.ifsp.boletofacil.builders.Boleto;
 import jocimar_paulo.ifsp.boletofacil.dados.Conexao;
 
 public class GeraBoleto extends AppCompatActivity {
 
-    TextView lblBoleto;
+    ImageView imvLogo;
+    EditText edtCodBarras;
+    EditText edtCedente;
+    EditText edtSacado;
+    EditText edtObs;
+    EditText edtNossoNum;
+    EditText edtVencto;
+    EditText edtValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gera_boleto);
 
-        lblBoleto = (TextView) findViewById(R.id.lblBoleto);
+        imvLogo = (ImageView) findViewById(R.id.imvLogo);
+        edtCodBarras = (EditText) findViewById(R.id.edtCodBarras);
+        edtCedente = (EditText) findViewById(R.id.edtCedente);
+        edtSacado = (EditText) findViewById(R.id.edtSacado);
+        edtObs = (EditText) findViewById(R.id.edtObs);
+        edtNossoNum = (EditText) findViewById(R.id.edtNossoNum);
+        edtVencto = (EditText) findViewById(R.id.edtVencto);
+        edtValor = (EditText) findViewById(R.id.edtValor);
 
-        //Boleto b = ActDadosSacado.boleto;
-        lblBoleto.setText(Conexao.boleto.toString());
+        popularBoleto();
 
+    }
+
+    public void popularBoleto(){
+
+        // Trocando a imagem do boleto
+
+        edtCodBarras.setText(Conexao.boleto.getCodBarras());
+        edtCedente.setText(Conexao.boleto.getCedente());
+        edtSacado.setText(Conexao.boleto.getSacado());
+        edtObs.setText(Conexao.boleto.getObservacao());
+        edtNossoNum.setText(String.valueOf(Conexao.boleto.getNossoNumero()));
+
+        // Conversão da data para uma visualização amigavél
+        Date data =  Conexao.boleto.getVencimento().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String dataFormatada = sdf.format(data);
+        edtVencto.setText(dataFormatada);
+
+        edtValor.setText("R$ " + String.valueOf(Conexao.boleto.getValor()));
+
+        trocarIconeBanco();
+
+    }
+
+    public void trocarIconeBanco(){
+        String codBarras = Conexao.boleto.getCodBarras();
+
+        // Identificando o banco pelo 3 primeiros caracteres do Cod. Barras
+        String codBanco = codBarras.substring(0,3);
+
+        if( codBanco.equals( "001" ) ){
+            imvLogo.setImageResource(R.drawable.logo_bb);
+        } else if (codBanco.equals( "341" ) ){
+            imvLogo.setImageResource(R.drawable.logo_itau);
+        }
     }
 }
